cbc2fb3255202bb23bd73fb16314b02998a1815a
seszcze <sebastian@ably.pl>
2018-04-26 22:54:58 +0200

    work in progress

---
+++ src/ModelsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LaPress\Models;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModelsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
