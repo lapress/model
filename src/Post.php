d8a719b642ae49f930e0a632ead9100c6159a6a2
Sebastian Szczepanski <sebastian@ably.pl>
2019-01-25 01:10:41 +0100

    further search integration

--- src/Post.php
+++ src/Post.php
@@ -2,10 +2,52 @@
 
 namespace LaPress\Models;
 
+use Laravel\Scout\Searchable;
+
 /**
  * @author    Sebastian Szczepański
  * @copyright ably
  */
 class Post extends AbstractPost
 {
+    use Searchable;
+
+    /**
+     * @var bool
+     */
+    public $asYouType = true;
+
+    /**
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        if ($this->isDraft()) {
+            return [];
+        }
+
+        return $this->getSearchableArray();
+    }
+
+    /**
+     * @return array
+     */
+    public function getSearchableArray(): array
+    {
+        return [
+            'ID'         => $this->ID,
+            'post_title' => $this->post_title,
+            'body'       => $this->post_content,
+            'categories' => $this->categories->pluck('name')->implode(', '),
+            'post_date'  => $this->date,
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function searchableAs(): string
+    {
+        return 'posts';
+    }
 }
