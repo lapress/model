5af5c7aa837913241a9bb06c39d33566bd5206b7
Sebastian Szczepanski <sebastian@ably.pl>
2019-02-01 15:13:27 +0100

    create add method for attachment

--- src/Attachment.php
+++ src/Attachment.php
@@ -2,6 +2,8 @@
 
 namespace LaPress\Models;
 
+use Carbon\Carbon;
+
 /**
  * @author    Sebastian Szczepański
  * @copyright ably
@@ -35,5 +37,57 @@ class Attachment extends AbstractPost
     public function getUrlAttribute(): ?string
     {
         return $this->guid;
+    }
+
+    /**
+     * @param       $data
+     * @param int   $width
+     * @param int   $height
+     * @param array $meta
+     * @return mixed
+     */
+    public static function add($data, int $width, int $height, $meta = [])
+    {
+        $now = Carbon::now();
+        $base = [
+            'post_content_filtered' => '',
+            'post_modified'         => $now,
+            'post_modified_gmt'     => $now->subHour(),
+            'to_ping'               => '',
+            'pinged'                => '',
+            'post_content'          => '',
+            'post_excerpt'          => '',
+        ];
+
+        $attachment = self::create(array_merge($base, $data));
+        $file = str_after($base['guid'] ?? '', config('wordpress.content.url'));
+        $meta = [
+            '_wp_attached_file'       => $file,
+            '_wp_attachment_metadata' => serialize([
+                'width'      => $width,
+                'height'     => $height,
+                'file'       => $file,
+                'sizes'      => [],
+                'image_meta' =>
+                    [
+                        'aperture'          => '0',
+                        'credit'            => '',
+                        'camera'            => '',
+                        'caption'           => '',
+                        'created_timestamp' => '0',
+                        'copyright'         => '',
+                        'focal_length'      => '0',
+                        'iso'               => '0',
+                        'shutter_speed'     => '0',
+                        'title'             => '',
+                        'orientation'       => '0',
+                        'keywords'          => [],
+                    ],
+            ])
+        ];
+
+        $attachment->saveMeta($meta);
+
+        return $attachment;
     }
 }
